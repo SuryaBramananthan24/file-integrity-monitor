aada310ec5822ba4f65c34e7af724d2969aa2843
Surya <suryasurya6444@gmail.com>
2026-08-29 20:31:46 +0530

    feat: initial commit

---
+++ src/main/java/com/devops/file_integrity_monitor/FileIntegrityMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.devops.file_integrity_monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileIntegrityMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileIntegrityMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/file_integrity_monitor/FileIntegrityMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.file_integrity_monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileIntegrityMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
